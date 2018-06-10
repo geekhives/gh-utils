23f07e5bac1c5c58a439d9f27de76376c1fb4d94
Jinggo Villamor <jingcleovil@gmail.com>
2018-06-10 09:26:20 +0800

    Remove history temporarily

--- build.js
+++ build.js
@@ -1,9 +1,7 @@
 import * as Helper from './src/Helper';
 import * as Service from './src/Service';
-import History from './src/History';
 
 export {
     Helper,
-    Service,
-    History
+    Service
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.History = exports.Service = exports.Helper = undefined;
+exports.Service = exports.Helper = undefined;
 
 var _Helper = require('./src/Helper');
 
@@ -13,14 +13,7 @@ var _Service = require('./src/Service');
 
 var Service = _interopRequireWildcard(_Service);
 
-var _History = require('./src/History');
-
-var _History2 = _interopRequireDefault(_History);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 exports.Helper = Helper;
 exports.Service = Service;
-exports.History = _History2.default;
